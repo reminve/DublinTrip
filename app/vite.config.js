6cb2a84cba2ebb18acdf3c741f3173be60b348e1
Rémi Neveu <melectrolux28@gmail.com>
2026-08-07 18:57:32 +0100

    fck agy

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react()
+    react(),
+    tailwindcss()
   ],
   base: './', // Ensures assets are resolved relatively, preventing 404s on GitHub Pages
 })
